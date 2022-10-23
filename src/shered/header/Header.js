c3159e17f18fc43953f418d8c195532a584424fa
Md Hasan <mdhasan8064@gmail.com>
2022-10-23 09:30:45 +0600

    finish 60

--- src/shered/header/Header.js
+++ src/shered/header/Header.js
@@ -2,27 +2,33 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import LeftSideNav from '../leftsideNav/LeftSideNav';
 
 const Header = () => {
     return (
-        <Navbar collapseOnSelect sticky='top' expand="lg" bg="light" variant="light" className='mb-2 py-3'>
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <section>
+            <Navbar collapseOnSelect sticky='top' expand="lg" bg="light" variant="light" className='mb-2 py-3'>
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                        <div className=''>
+                            <LeftSideNav />
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </section>
     );
 };
 
